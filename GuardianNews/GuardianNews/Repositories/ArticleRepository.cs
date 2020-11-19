fc7e7e3cf0a1b5f8750980305ed15754b3e5efe8
MaciejBiernat <57531118+MaciejBiernat@users.noreply.github.com>
2020-11-19 16:10:07 +0100

    Implement Article Repository

---
+++ GuardianNews/GuardianNews/Repositories/ArticleRepository.cs
@@ -0,0 +1,39 @@
+﻿using GuardianNews.Data;
+using GuardianNews.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuardianNews.Repositories
+{
+    public class ArticleRepository : IRepository<Article>
+    {
+        private readonly GuardianContext _context;
+        private DbSet<Article> table;
+        public ArticleRepository(GuardianContext context)
+        {
+            _context = context;
+            table = _context.Set<Article>();
+        }
+
+        public async void AddAsync(Article entity)
+        {
+            await _context.AddAsync(entity);
+            _context.SaveChanges();
+        }
+
+        public async void AddRangeAsync(Article[] entities)
+        {
+            await _context.AddRangeAsync(entities);
+            _context.SaveChanges();
+        }
+
+        public async Task<Article[]> GetAllAsync()
+        {
+            return await table.ToArrayAsync();
+        }
+    }
+}
---
+++ GuardianNews/GuardianNews/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuardianNews.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        void AddAsync(T entity);
+
+        void AddRangeAsync(T[] entity);
+
+        Task<T[]> GetAllAsync();
+
+    }
+}
